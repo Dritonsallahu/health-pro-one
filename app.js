73c7f3c32ac67160e90f5925afc445782985114b
dritonsallahu <dritonsallahu2@gmail.com>
2023-09-10 20:53:56 +0200

    Login UI updated

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require("path");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "assets")));
+console.log(path.join(__dirname, "assets"));
 
 app.use("/", login);
 app.use("/superadmin", superadmin);
